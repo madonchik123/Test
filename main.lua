b7dc52d07fdc5b35557cd967c08ebd5acd721a8f
madonchik123 <68397448+madonchik123@users.noreply.github.com>
2022-08-18 15:30:43 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,68 +2,6 @@
 repeat
     wait()
 until game:IsLoaded()
-local PathfindingService = game:GetService("PathfindingService")
-local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
- 
-local path = PathfindingService:CreatePath()
- 
-local player = Players.LocalPlayer
-local character = player.Character
-local humanoid = character:WaitForChild("Humanoid")
- 
-local waypoints
-local nextWaypointIndex
-local reachedConnection
-local blockedConnection
- 
-local function followPath(destination)
-	-- Compute the path
-	local success, errorMessage = pcall(function()
-		path:ComputeAsync(character.PrimaryPart.Position, destination)
-	end)
- 
-	if success and path.Status == Enum.PathStatus.Success then
-		-- Get the path waypoints
-		waypoints = path:GetWaypoints()
- 
-		-- Detect if path becomes blocked
-		blockedConnection = path.Blocked:Connect(function(blockedWaypointIndex)
-			-- Check if the obstacle is further down the path
-			if blockedWaypointIndex >= nextWaypointIndex then
-				-- Stop detecting path blockage until path is re-computed
-				blockedConnection:Disconnect()
-				-- Call function to re-compute new path
-				followPath(destination)
-			end
-		end)
- 
-		-- Detect when movement to next waypoint is complete
-		if not reachedConnection then
-			reachedConnection = humanoid.MoveToFinished:Connect(function(reached)
-				if reached and nextWaypointIndex < #waypoints then
-					-- Increase waypoint index and move to next waypoint
-					nextWaypointIndex += 1
-					humanoid:MoveTo(waypoints[nextWaypointIndex].Position)
-				else
-					reachedConnection:Disconnect()
-					blockedConnection:Disconnect()
-				end
-			end)
-		end
- 
-		-- Initially move to second waypoint (first waypoint is path start; skip it)
-		nextWaypointIndex = 2
-		humanoid:MoveTo(waypoints[nextWaypointIndex].Position)
-	else
-		warn("Path not computed!", errorMessage)
-	end
-end
-for i,v in pairs(game.Workspace:GetDescendants()) do
-if string.find(v.ClassName,"Seat" then
-v:Destroy()
-end
-end
 --Stops script if on a different game
 if game.PlaceId ~= 8737602449 then
     return
@@ -127,6 +65,68 @@ local booths = {
 }
 local queueonteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
 local httprequest = (syn and syn.request) or http and http.request or http_request or (fluxus and fluxus.request) or request
+local PathfindingService = game:GetService("PathfindingService")
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+ 
+local path = PathfindingService:CreatePath()
+ 
+local player = Players.LocalPlayer
+local character = player.Character
+local humanoid = character:WaitForChild("Humanoid")
+ 
+local waypoints
+local nextWaypointIndex
+local reachedConnection
+local blockedConnection
+ 
+local function followPath(destination)
+	-- Compute the path
+	local success, errorMessage = pcall(function()
+		path:ComputeAsync(character.PrimaryPart.Position, destination)
+	end)
+ 
+	if success and path.Status == Enum.PathStatus.Success then
+		-- Get the path waypoints
+		waypoints = path:GetWaypoints()
+ 
+		-- Detect if path becomes blocked
+		blockedConnection = path.Blocked:Connect(function(blockedWaypointIndex)
+			-- Check if the obstacle is further down the path
+			if blockedWaypointIndex >= nextWaypointIndex then
+				-- Stop detecting path blockage until path is re-computed
+				blockedConnection:Disconnect()
+				-- Call function to re-compute new path
+				followPath(destination)
+			end
+		end)
+ 
+		-- Detect when movement to next waypoint is complete
+		if not reachedConnection then
+			reachedConnection = humanoid.MoveToFinished:Connect(function(reached)
+				if reached and nextWaypointIndex < #waypoints then
+					-- Increase waypoint index and move to next waypoint
+					nextWaypointIndex += 1
+					humanoid:MoveTo(waypoints[nextWaypointIndex].Position)
+				else
+					reachedConnection:Disconnect()
+					blockedConnection:Disconnect()
+				end
+			end)
+		end
+ 
+		-- Initially move to second waypoint (first waypoint is path start; skip it)
+		nextWaypointIndex = 2
+		humanoid:MoveTo(waypoints[nextWaypointIndex].Position)
+	else
+		warn("Path not computed!", errorMessage)
+	end
+end
+for i,v in pairs(game.Workspace:GetDescendants()) do
+if string.find(v.ClassName,"Seat" then
+v:Destroy()
+end
+end
 queueonteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/madonchik123/Test/main/main.lua'))()")
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/tzechco/roblox-scripts/main/UI/ui-engine-v2.lua"))()
 getgenv().settings = {}
