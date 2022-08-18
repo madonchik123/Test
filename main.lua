675bbffa5fbecd19a48db6731b95c70de7d35c3a
madonchik123 <68397448+madonchik123@users.noreply.github.com>
2022-08-18 15:28:57 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,68 @@
 repeat
     wait()
 until game:IsLoaded()
-
+local PathfindingService = game:GetService("PathfindingService")
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+ 
+local path = PathfindingService:CreatePath()
+ 
+local player = Players.LocalPlayer
+local character = player.Character
+local humanoid = character:WaitForChild("Humanoid")
+ 
+local waypoints
+local nextWaypointIndex
+local reachedConnection
+local blockedConnection
+ 
+local function followPath(destination)
+	-- Compute the path
+	local success, errorMessage = pcall(function()
+		path:ComputeAsync(character.PrimaryPart.Position, destination)
+	end)
+ 
+	if success and path.Status == Enum.PathStatus.Success then
+		-- Get the path waypoints
+		waypoints = path:GetWaypoints()
+ 
+		-- Detect if path becomes blocked
+		blockedConnection = path.Blocked:Connect(function(blockedWaypointIndex)
+			-- Check if the obstacle is further down the path
+			if blockedWaypointIndex >= nextWaypointIndex then
+				-- Stop detecting path blockage until path is re-computed
+				blockedConnection:Disconnect()
+				-- Call function to re-compute new path
+				followPath(destination)
+			end
+		end)
+ 
+		-- Detect when movement to next waypoint is complete
+		if not reachedConnection then
+			reachedConnection = humanoid.MoveToFinished:Connect(function(reached)
+				if reached and nextWaypointIndex < #waypoints then
+					-- Increase waypoint index and move to next waypoint
+					nextWaypointIndex += 1
+					humanoid:MoveTo(waypoints[nextWaypointIndex].Position)
+				else
+					reachedConnection:Disconnect()
+					blockedConnection:Disconnect()
+				end
+			end)
+		end
+ 
+		-- Initially move to second waypoint (first waypoint is path start; skip it)
+		nextWaypointIndex = 2
+		humanoid:MoveTo(waypoints[nextWaypointIndex].Position)
+	else
+		warn("Path not computed!", errorMessage)
+	end
+end
+for i,v in pairs(game.Workspace:GetDescendants()) do
+if string.find(v.ClassName,"Seat" then
+v:Destroy()
+end
+end
 --Stops script if on a different game
 if game.PlaceId ~= 8737602449 then
     return
@@ -656,28 +717,28 @@ if mostDonated == true then
     for i, v in next, Players:GetPlayers() do
         if v.Name ~= lp.Name and v.leaderstats:WaitForChild("Donated").Value > minimumDonated then
             
-	lp.Character.Humanoid:MoveTo(v.Character.HumanoidRootPart.Position)
+	followPath(v.Character.HumanoidRootPart.Position)
             wait(4)
             game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(
                 msgs[math.random(1, #msgs)],
                 "All"
             )
         wait(5)
-	Players.LocalPlayer.Character.Humanoid:MoveTo(Vector3.new(booths[tostring(unclaimed[1])]:match("(.+), (.+), (.+)")))
+	followPath(Vector3.new(booths[tostring(unclaimed[1])]:match("(.+), (.+), (.+)")))
         wait(cooldown)
         end
     end
 elseif mostDonated == false then
     for i, v in next, Players:GetPlayers() do
         if v.Name ~= lp.Name then
-            lp.Character.Humanoid:MoveTo(v.Character.HumanoidRootPart.Position)
+            followPath(v.Character.HumanoidRootPart.Position)
             wait(4)
             game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(
                 msgs[math.random(1, #msgs)],
                 "All"
             )
     	    wait(5)
-	    Players.LocalPlayer.Character.Humanoid:MoveTo(Vector3.new(booths[tostring(unclaimed[1])]:match("(.+), (.+), (.+)")))
+	    followPath(Vector3.new(booths[tostring(unclaimed[1])]:match("(.+), (.+), (.+)")))
             wait(cooldown)
         end
     end
